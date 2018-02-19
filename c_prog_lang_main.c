1b7d4ce374c0e299aaa7e672770aece0005ea6ce
Mihai Constantin <constant.mihai@googlemail.com>
2018-02-19 21:08:13 +0100

    Initial Commit

---
+++ c_prog_lang_main.c
@@ -0,0 +1,34 @@
+/***************************************** 
+
+* File Name : main.c
+
+* Creation Date : 19-02-2018
+
+* Last Modified :
+
+* Created By : Mihai Constantin constant.mihai@googlemail.com
+
+* License :
+
+******************************************/
+
+/**
+ *  Purpose:
+ *
+ */
+
+// Config
+#include "c_prog_lang_config.h"
+
+#if USE_FAHRENHEIT_TABLE
+#include "fahrenheit_table.h"
+#endif
+#include <stdio.h>
+
+int main(int argc, char ** argv) {
+    printf("Hello world\n");
+#if USE_FAHRENHEIT_TABLE
+    printf("This should be a table header");
+    int ret = fahrenheit_table();
+#endif
+}
---
+++ fahrenheit_table/fahrenheit_table.h
@@ -0,0 +1,26 @@
+/****************************************** 
+
+* File Name : fahrenheit_table.h
+
+* Creation Date : 19-02-2018
+
+* Last Modified :
+
+* Created By : Mihai Constantin constant.mihai@googlemail.com
+
+* License : 
+
+******************************************/
+
+/**
+ * Purpose
+ */
+
+#ifndef _FAHRENHEIT_TABLE_H_
+#define _FAHRENHEIT_TABLE_H_
+
+
+int fahrenheit_table(void);
+
+
+#endif
